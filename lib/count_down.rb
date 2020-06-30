d7456ca4df2e2f1b03ef30aa57e07a83f71d113d
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-06-30 20:56:35 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,8 @@
-# Write your code here
+countdown_numb = 0
+count = 10
+while count > 0 do
+  break if count == countdown_numb
+    puts "#{count}\n"
+  count = count- 1 
+end
+puts "Happy New Year!"
