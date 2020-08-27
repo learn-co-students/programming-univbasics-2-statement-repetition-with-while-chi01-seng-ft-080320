62f108ffd799f19ed79c0f9cdfa044ce98df07aa
lindsayMecher <lindsaymecher@gmail.com>
2020-08-27 09:51:49 -0500

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,12 @@
 # Write your code here
+
+counter = 10
+while counter >= 0 do
+    if counter == 0
+        puts "Happy New Year!"
+    else
+        puts counter 
+    end
+    counter -= 1
+end
+
